f284513a7990ffda564241c9fb3d3a9d17d35d31
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-08-30 10:40:15 +0300

    portal sekous

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/PortalManager.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/PortalManager.cs
@@ -5,9 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class PortalManager : MonoBehaviour
 {
+    public GameObject player;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Object.DontDestroyOnLoad(player);
+        player.transform.position = new Vector2(0, 0);
         SceneManager.LoadScene("BlackForest");
     }
 }
---
+++ Assets/spawnBack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class spawnBack : MonoBehaviour
+{
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Object.DontDestroyOnLoad(player);
+        player.transform.position = new Vector2(50.5f, 28f);
+        SceneManager.LoadScene("Trials");
+    }
+}
