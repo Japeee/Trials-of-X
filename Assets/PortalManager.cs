8dc21d6b376fb07a1bc170617c877f58638384e6
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-09-06 12:13:59 +0300

    Portal viimeistely

---
+++ Assets/PortalManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalManager : MonoBehaviour
+{
+    public GameObject player, portal;
+
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(Teleport());
+        }
+    }
+
+    IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(0.5f);
+        player.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y);
+    }
+}
