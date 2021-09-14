3a40a34eadcfdcf36fbc4734c0013793f4cf2dbc
Japeee <74297488+Japeee@users.noreply.github.com>
2021-09-14 12:34:04 +0300

    Upp

---
+++ Assets/Jape/DestroyBullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBullet : MonoBehaviour
+{
+   // public GameObject hitEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject, 3f);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Jape/FirePointFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePointFollow : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+    private Vector3 targetPos;
+    public float lerpSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPos, lerpSpeed * Time.deltaTime);
+    }
+}
