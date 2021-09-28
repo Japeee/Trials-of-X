df556f4506ada95a53487d4e40801339dff30f2d
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-09-28 13:57:17 +0300

    puskun

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject bag;
+    private bool isOpened;
+    private void Start()
+    {
+        isOpened = false;
+        bag = GameObject.Find("ItemParent");
+        //GameObject.Find("ItemParent").SetActive(false);
+    }
+
+    private void OnButtonClick()
+    {
+        if (isOpened == false)
+        {
+            bag.SetActive(true);
+            isOpened = true;
+        }
+        else if (isOpened == true)
+        {
+            bag.SetActive(false);
+            isOpened = false;
+        }
+    }
+}
