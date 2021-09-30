539a58a8b220931f0fe869fc9c2c2c0e6281501a
Japeee <74297488+Japeee@users.noreply.github.com>
2021-09-30 09:16:30 +0300

    Up

--- Assets/Jape/Scripts/DestroyBullet.cs
+++ Assets/Jape/Scripts/DestroyBullet.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class DestroyBullet : MonoBehaviour
 {
-   // public GameObject hitEffect;
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
         Destroy(gameObject, 3f);
         Destroy(gameObject);
     }
