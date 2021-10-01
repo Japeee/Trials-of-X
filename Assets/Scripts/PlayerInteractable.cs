91125eb5766f136d691eb58af9796f34acd61a56
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-10-01 10:26:42 +0300

    upupup

--- Assets/Scripts/PlayerInteractable.cs
+++ Assets/Scripts/PlayerInteractable.cs
@@ -4,20 +4,5 @@ using UnityEngine;
 
 public class PlayerInteractable : MonoBehaviour
 {
-    Interactable radius; 
-
-
-    void Start()
-    {
-         = GetComponent<Interactable>();
-    }
-
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(1) && radius < 3f)
-        {
-           
-        }
-    }
+   
 }
