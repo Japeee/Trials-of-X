e71837b5dfdf0626d0d93f059d2a072281f72cea
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-10-07 10:58:27 +0300

    Healthbar ja inventory :white_check_mark:

---
+++ Assets/Scripts/Slots.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slots : MonoBehaviour
+{
+
+    private Inventory inventory;
+    public int i;
+
+    void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    void Update()
+    {
+        if (transform.childCount <= 0)
+        {
+            inventory.isFull[i] = false;
+        }
+    }
+
+    public void DropItem()
+    {
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<SpawnItem>().SpawnDroppedItem();
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
+
+    
+}
