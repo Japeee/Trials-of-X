2f71d35ea9a826f41bb7ac91c76f191b13cfdc1a
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-10-18 12:43:18 +0300

    up

--- Assets/Scripts/ItemDataBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Item Database", menuName = "Item/Item Database")]
-public class ItemDataBase : ScriptableObject
-{
-    public List<Items> allItems;
-}
